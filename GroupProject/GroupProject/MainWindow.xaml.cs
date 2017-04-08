1cea69e654858d79d51ab06692a2181e676b9f34
Tyler Parkinson <Tyler Parkinson>
2017-04-08 16:36:30 -0600

    Added events and comments for main window

--- GroupProject/GroupProject/MainWindow.xaml.cs
+++ GroupProject/GroupProject/MainWindow.xaml.cs
@@ -27,12 +27,33 @@ namespace GroupProject
 
         private void btn_search_click(object sender, RoutedEventArgs e)
         {
+            //this will open the search window. Will need a constructor on the search window that accepts
+            //an out variable to be set to the id to populate in the main window
             new SearchWindow().ShowDialog();
         }
 
         private void btn_product_click(object sender, RoutedEventArgs e)
         {
+            //no data will need to be passed between these two objects, but both should have access to the static manger
+            //class that will hold the datastructures with the invoice and product data as well as 
+            //comunicate with the db class
             new Prodcut().ShowDialog();
         }
+
+        private void btn_add_invoice_click(object sender, RoutedEventArgs e)
+        {
+            //insert new invoiced data into the db. this will probalby be done through a manager class to avoid work on the ui
+        }
+
+        private void btn_edit_invoice_click(object sender, RoutedEventArgs e)
+        {
+            //update current invoice in db. This will proably be done by passing the relavant info into a manager class
+        }
+
+        private void btn_delete_invoice_click(object sender, RoutedEventArgs e)
+        {
+            //delete the current invoice. this will probalby be done in some object manager class that you pass the id
+        }
+
     }
 }
