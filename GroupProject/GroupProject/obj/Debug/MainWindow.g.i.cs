e8c53cd35294cb5ab4c93019831053c8a110f211
npreszler <npreszler@gmail.com>
2017-04-07 20:56:34 -0600

    Added Search Window mockup. No event handlers.

--- GroupProject/GroupProject/obj/Debug/MainWindow.g.i.cs
+++ GroupProject/GroupProject/obj/Debug/MainWindow.g.i.cs
@@ -40,22 +40,6 @@ namespace GroupProject {
     /// </summary>
     public partial class MainWindow : System.Windows.Window, System.Windows.Markup.IComponentConnector {
         
-        
-        #line 10 "..\..\MainWindow.xaml"
-        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
-        internal System.Windows.Controls.Label label;
-        
-        #line default
-        #line hidden
-        
-        
-        #line 12 "..\..\MainWindow.xaml"
-        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
-        internal System.Windows.Controls.Label label1;
-        
-        #line default
-        #line hidden
-        
         private bool _contentLoaded;
         
         /// <summary>
@@ -84,15 +68,6 @@ namespace GroupProject {
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1800:DoNotCastUnnecessarily")]
         void System.Windows.Markup.IComponentConnector.Connect(int connectionId, object target) {
-            switch (connectionId)
-            {
-            case 1:
-            this.label = ((System.Windows.Controls.Label)(target));
-            return;
-            case 2:
-            this.label1 = ((System.Windows.Controls.Label)(target));
-            return;
-            }
             this._contentLoaded = true;
         }
     }
